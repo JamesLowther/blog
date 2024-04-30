882af086dd9ae8448c10a5f58952bff5a4152610
James Lowther <james@jameslowther.com>
2024-04-30 15:48:30 -0600

    Quartz sync: Apr 30, 2024, 3:48 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,7 +8,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}><img style="height: 1em; margin: 0; margin-right: 17px; vertical-align: -10%;" src="/static/icon.png" />{title}</a>
     </h1>
   )
 }
